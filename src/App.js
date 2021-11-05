0478ec7ed8f3198d2ee82903b8517a9fb6dae719
Matheus Silveira <matheuspmsilveira@gmail.com>
2021-11-05 17:01:12 -0300

    impl: routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
+    <div className="app">
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/comidas" />
+        <Route path="/bebidas" />
+        <Route path={ `/comidas/${id - da - receita}` } />
+        <Route path={ `/bebidas/${id - da - receita}` } />
+        <Route path={ `/comidas/${id - da - receita}/in-progress` } />
+        <Route path={ `/bebidas/${id - da - receita}/in-progress` } />
+        <Route path="/explorar" />
+        <Route path="/explorar/comidas" />
+        <Route path="/explorar/bebidas" />
+        <Route path="/explorar/comidas/ingredientes" />
+        <Route path="/explorar/bebidas/ingredientes" />
+        <Route path="/explorar/comidas/area" />
+        <Route path="/perfil" />
+        <Route path="/receitas-feitas" />
+        <Route path="/receitas-favoritas" />
+      </Switch>
     </div>
   );
 }
