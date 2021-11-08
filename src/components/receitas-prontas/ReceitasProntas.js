1859fb70f9e07cd31031395943c88574d55d961a
Danilo <danilo.santossouza@outlook.com>
2021-11-08 19:06:51 -0300

    Imple: Pagina de Receitas prontas

---
+++ src/components/receitas-prontas/ReceitasProntas.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export default function ReceitasProntas({ receitasProntas }) {
+  return (
+    <div>
+      { receitasProntas.map((item, index) => (
+        <>
+          <img src="" alt="" data-testid={ `${index}-horizontal-image` } />
+          <h2 data-testid={ `${index}-horizontal-top-text` }>{item.category}</h2>
+          <h2 data-testid={ `${index}-horizontal-name` }>{item.name}</h2>
+          <h2 data-testid={ `${index}-horizontal-done-date` }>{item.doneDate}</h2>
+          <button
+            data-testid={ `${index}-horizontal-share-btn` }
+            type="button"
+          >
+            Compartilhar
+          </button>
+          <p data-testid={ `${index}-${item.tags}-horizontal-tag` }>{item.tags}</p>
+        </>
+      ))}
+    </div>
+  );
+}
+
+ReceitasProntas.propTypes = {
+  array: PropTypes.shape({
+    map: PropTypes.func,
+  }),
+}.isRequired;
--- src/pages/ReceitasFeitas.jsx
+++ src/pages/ReceitasFeitas.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import ReceitasProntas from '../components/receitas-prontas/ReceitasProntas';
 
 export default function ReceitasFeitas() {
+  const { receitasFeitas, setReceitasFeitas } = useState([]);
+  useEffect(() => {
+    const getLocalStorageDoneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
+    setReceitasFeitas(getLocalStorageDoneRecipes);
+  }, []);
   return (
     <div>
-      Receitas Feitas
+      <button data-testid="filter-by-all-btn" type="button">All</button>
+      <button data-testid="filter-by-food-btn" type="button">Food</button>
+      <button data-testid="filter-by-drink-btn" type="button">Drinks</button>
+      <ReceitasProntas receitasProntas={ receitasFeitas } />
     </div>
   );
 }
