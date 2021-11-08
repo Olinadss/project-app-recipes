b224418dcc886c99f1088b306c464249e661d461
Matheus Silveira <matheuspmsilveira@gmail.com>
2021-11-08 16:21:46 -0300

    impl: bebidas provider no App

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Routes from './Routes';
 import { ComidasProvider } from './hooks/useComidas';
+import { BebidasProvider } from './hooks/useBebidas';
 
 function App() {
   return (
     <div className="app">
       <ComidasProvider>
-        <Routes />
+        <BebidasProvider>
+          <Routes />
+        </BebidasProvider>
       </ComidasProvider>
     </div>
   );
