55d710aa5645d9887efefdf412d907a7d17904a6
gbezzuoli <guilhermebezzuoli@outlook.com>
2021-11-08 13:50:03 -0300

    req 9

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../components/Header';
 
 export default function Recipes() {
   return (
     <div>
+      <Header />
       Tela de receitas
     </div>
   );
