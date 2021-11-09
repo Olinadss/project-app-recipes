bf07fad2d36edea59b078e74cd2e9e1bfbf90a62
Danilo <danilo.santossouza@outlook.com>
2021-11-09 15:44:36 -0300

    Ajuste Lint

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Switch, Route } from 'react-router';
 import Routes from './Routes';
 import { ComidasProvider } from './hooks/useComidas';
 import { BebidasProvider } from './hooks/useBebidas';
 import GlobalStorage from './context/GlobalStorage';
 
-
-
 function App() {
   return (
     <div className="app">
-      <Switch>
       <GlobalStorage>
-      <ComidasProvider>
-        <BebidasProvider>
-          <Routes />
-        </BebidasProvider>
-      </ComidasProvider>
-    </GlobalStorage>
-    </Switch>
+        <ComidasProvider>
+          <BebidasProvider>
+            <Routes />
+          </BebidasProvider>
+        </ComidasProvider>
+      </GlobalStorage>
     </div>
   );
 }
