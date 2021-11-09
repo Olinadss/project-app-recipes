f46a6cc89799364f9a5768954f3bd4a85e5382b8
Matheus Silveira <matheuspmsilveira@gmail.com>
2021-11-09 16:34:51 -0300

    impl: requisito 82

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -5,7 +5,22 @@ export default function Perfil() {
   return (
     <div>
       <Header title="Perfil" search={ false } />
-      Perfil
+      <section>
+        <p data-testid="profile-email">email@gmail.com</p>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+        >
+          Receitas Feitas
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+        >
+          Receitas Favoritas
+        </button>
+        <button type="button" data-testid="profile-logout-btn">Sair</button>
+      </section>
       <Footer />
     </div>
   );
