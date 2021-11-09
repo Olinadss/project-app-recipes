fcdcfa783e12a7d72ea08bc5af9645ca81b761db
Matheus Silveira <matheuspmsilveira@gmail.com>
2021-11-09 17:49:58 -0300

    impl: requisito 69

--- src/pages/Explorar.jsx
+++ src/pages/Explorar.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { Header, Footer } from '../components';
 
 export default function Explorar() {
+  const history = useHistory();
+
+  function handleClick(route) {
+    history.push(route);
+  }
+
   return (
     <div>
       <Header title="Explorar" search={ false } />
       <button
         data-testid="explore-food"
         type="button"
+        onClick={ () => handleClick('/explorar/comidas') }
       >
         Explorar Comidas
       </button>
       <button
         data-testid="explore-drinks"
         type="button"
+        onClick={ () => handleClick('/explorar/bebidas') }
       >
         Explorar Bebidas
       </button>
