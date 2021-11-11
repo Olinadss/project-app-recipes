4c82a98d43f2349a38d6b7ad075da892e1c0222b
Renato Bispo <renatoslvbispo@gmail.com>
2021-11-11 15:49:46 -0300

    Conserta erros do lint

--- src/hooks/useCopyRecipeLinkToClipboard.js
+++ src/hooks/useCopyRecipeLinkToClipboard.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import copy from 'clipboard-copy';
 
 export default function useCopyRecipeLinkToClipboard(parentPath, recipeID) {
