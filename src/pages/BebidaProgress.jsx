e4ceaef0b02cbc57bf218fe7e98bd45ac28e4fc5
Renato Bispo <renatoslvbispo@gmail.com>
2021-11-12 03:47:14 -0300

    Remove código comentado

--- src/pages/BebidaProgress.jsx
+++ src/pages/BebidaProgress.jsx
@@ -38,42 +38,6 @@ export default function BebidaProgress() {
     );
   }, [ingredients]);
 
-  // useEffect(() => {
-  //   async function fetchFilteredMeal() {
-  //     const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idCocktails}`);
-  //     const data = await response.json();
-  //     const drink = data.drinks[0];
-
-  //     setIngredientsInfo(drink);
-  //     setIngredientsAndMeasures(getIngredientsWithMeasures(drink));
-
-  //     const ingredientList = Object.keys(getIngredientsWithMeasures(drink));
-
-  //     const inProgressRecipes = getLocalStorage('inProgressRecipes');
-
-  //     let drinksToLocalStorage = null;
-
-  //     if (inProgressRecipes) {
-  //       drinksToLocalStorage = {
-  //         cocktails: {
-  //           ...inProgressRecipes.cocktails, [idCocktails]: ingredientList,
-  //         },
-  //       };
-  //     } else {
-  //       drinksToLocalStorage = {
-  //         cocktails: {
-  //           [idCocktails]: ingredientList,
-  //         },
-  //       };
-  //     }
-
-  //     setLocalStorage(
-  //       'inProgressRecipes', { ...inProgressRecipes, ...drinksToLocalStorage },
-  //     );
-  //   }
-  //   fetchFilteredMeal();
-  // }, []);
-
   return (
     ingredients
       ? (
